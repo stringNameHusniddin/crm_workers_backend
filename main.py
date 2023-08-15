66d64fbb116e9089e5fff7e75b30f731cc0e31fd
stringNameHusniddin <stringhusniddin@gmail.com>
2023-08-15 10:41:15 +0500

    change origin

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 origins = [
-    "https://crm-workers.vercel.app/",
+    "https://crm-workers.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
